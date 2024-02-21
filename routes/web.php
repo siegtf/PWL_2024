3a22cb0ac9239e81b1461c25be8cc4341578c632
= siegtf <= muhdsf1724@gmail.com>
2024-02-21 10:25:14 +0700

    Selamat Datang

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/hello', function () {
-    return view('Hello World');
+   return 'Hello World';
 });
+*/
+
+// Route::get('/world', function () {
+//  return 'World';
+// });
+
+Route::get('/welcome', function () {
+   return 'Selamat Datang';
+  });
